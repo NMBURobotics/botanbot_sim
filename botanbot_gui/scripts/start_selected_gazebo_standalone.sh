c3d5afae5997f694958eefeeddd477ea7a01ed24
jediofgever <fetulahatas1@gmail.com>
2021-05-28 09:29:57 +0800

    update paths

--- botanbot_gui/scripts/start_selected_gazebo_standalone.sh
+++ botanbot_gui/scripts/start_selected_gazebo_standalone.sh
@@ -5,7 +5,7 @@ source /opt/ros/foxy/setup.bash
 source $current_dir/../../../install/setup.bash
 source ~/.bashrc
 killall gzserver
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH$current_dir/../../../src/vox_nav/botanbot_gazebo/models
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH$current_dir/../../../src/botanbot_sim/botanbot_gazebo/models
 export GAZEBO_WORLD=$GAZEBO_WORLD$1
 ros2_launch_command="ros2 launch botanbot_gazebo botanbot_start_selected_gazebo_world.launch.py; bash"
 gnome-terminal -- sh -c "$ros2_launch_command"
