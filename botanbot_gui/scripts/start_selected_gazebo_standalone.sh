aaaf2bfe6c0934c45529480b7791faa608a89da2
jediofgever <fetulahatas1@gmail.com>
2022-03-18 22:04:43 +0100

    corrections related to GAZEBO_MODEL_PATH

--- botanbot_gui/scripts/start_selected_gazebo_standalone.sh
+++ botanbot_gui/scripts/start_selected_gazebo_standalone.sh
@@ -6,7 +6,7 @@ source $current_dir/../../../install/setup.bash
 source ~/.bashrc
 killall gzserver
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH$current_dir/../../../src/botanbot_sim/botanbot_gazebo/models
-export GAZEBO_MODEL_PATH=$current_dir/../../../src/ros2_full_sensor_suite/models:$GAZEBO_MODEL_PATH
+export GAZEBO_MODEL_PATH=$current_dir/../../../src/botanbot_sim/ros2_full_sensor_suite/models:$GAZEBO_MODEL_PATH
 export GAZEBO_WORLD=$GAZEBO_WORLD$1
 ros2_launch_command="ros2 launch botanbot_gazebo botanbot_start_selected_gazebo_world.launch.py; bash"
 gnome-terminal -- sh -c "$ros2_launch_command"
